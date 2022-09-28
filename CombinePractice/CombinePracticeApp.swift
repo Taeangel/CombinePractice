d834afb4f56d9743149353877add87584e034d75
Taeangel <wngks2050@gmail.com>
2022-09-28 20:32:50 +0900

    Initial Commit

---
+++ CombinePractice/CombinePracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombinePracticeApp.swift
+//  CombinePractice
+//
+//  Created by song on 2022/09/28.
+//
+
+import SwiftUI
+
+@main
+struct CombinePracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
